ad32035e0e824e2fcd2840726ffd97f7f357007c
2myungho <dl_audgh@naver.com>
2022-08-05 01:45:52 +0000

    ui eslint + prettier 적용
    id config 변경
    
    되돌리기

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  plugins: ['jest', 'prettier', 'react', 'react-hooks', 'jsx-a11y', 'import'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-param-reassign': ['error', { props: false }],
+  },
+};
